a929a1489be6b239e6510d408b1dceea5f0ce9bc
kadir-zeren <k4dzeren@gmail.com>
2024-12-27 16:22:42 +0300

    python example

---
+++ python.py
@@ -0,0 +1,69 @@
+print('Hello world')
+
+print('Benim yazdigim ilk kod')
+
+print()
+
+print("Cift tirnak yazdim")
+
+print('''uc tirnak ile yazdim''')
+
+print("I'm great")
+
+print("bir cumle")
+
+print('''"hi" he said''')
+
+print('"hi" he said')
+
+print('''"Don't do it" said the manager''')
+
+print('''bu birinci satir
+bu ikinci satir 
+bu uc 
+bu dort''')
+
+
+print('''bu birinci 'satir
+bu ikinci satir"
+bu uc 
+bu dort''')
+
+print('3.14')
+print(3.14)
+
+print('3.14'*3)
+print(3.14*3)
+
+print('abcd' * 2)
+print(5*2)
+
+print(567)
+
+print('bir')
+print()
+print('iki')
+
+print()
+print('')
+
+print()
+print("")
+print('''''')
+
+print('I\'m great')
+
+print("\"don't do it\" said the manager")
+
+print('bu benim cumlem \nbu ikinci satir')
+
+print('bu benim cumlem \tbu ikinci satir')
+
+print('laksdlksaldkasldk lakdlas kdlaskdlaskdl;kas \
+      lskakdlsakdla\
+      l;sald;aslda')
+
+# buradaki fonksiyon kare alma fonksiyonu
+print('kullanici ismi') # Burada kullanici ismi yazdiriliyor
+
+x = 5 # burada x degeri atandi
